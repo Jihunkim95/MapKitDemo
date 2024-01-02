386db524f7a57968627b8f525883c728ec9b209a
Jihunkim95 <darak551@naver.com>
2024-01-02 13:42:07 +0900

    init: 초기설정

---
+++ MapKitDemo/MapKitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitDemo
+//
+//  Created by 김지훈 on 2024/01/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitDemo/MapKitDemo/MapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitDemoApp.swift
+//  MapKitDemo
+//
+//  Created by 김지훈 on 2024/01/02.
+//
+
+import SwiftUI
+
+@main
+struct MapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
